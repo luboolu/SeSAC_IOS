1fda3837b1e7b8eabb63e550471c3118fd252437
luboolu <kjjy98@naver.com>
2021-09-29 22:14:02 +0900

    first commit

---
+++ ASSIGNMENT/SSAC_NETFLIX/SSAC_NETFLIX/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SSAC_NETFLIX
+//
+//  Created by 김진영 on 2021/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
