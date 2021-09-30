6a68fb2294d6faa96cdbf96095d3d0908c222d42
luboolu <kjjy98@naver.com>
2021-09-30 19:35:22 +0900

    first commit

--- ASSIGNMENT/SSAC_NETFLIX/SSAC_NETFLIX/SignUpViewController.swift
+++ ASSIGNMENT/SSAC_NETFLIX/SSAC_NETFLIX/SignUpViewController.swift
@@ -18,8 +18,10 @@ class SignUpViewController: UIViewController {
     //Button
     @IBOutlet var signUpButton: UIButton!
     @IBOutlet var addInfoButton: UIButton!
-    
+    //Switch
     @IBOutlet var hideTextFieldSwitch: UISwitch!
+    
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,32 +32,43 @@ class SignUpViewController: UIViewController {
         emailNumTextField.attributedPlaceholder = NSAttributedString(string: "이메일 주소 또는 전화번호", attributes: [.foregroundColor: UIColor.white])
         emailNumTextField.keyboardType = .emailAddress
         emailNumTextField.textAlignment = .center
+        emailNumTextField.borderStyle = .roundedRect
         
         passwordTextField.backgroundColor = .darkGray
         passwordTextField.attributedPlaceholder = NSAttributedString(string: "비밀번호", attributes: [.foregroundColor: UIColor.white])
         passwordTextField.isSecureTextEntry = true
         passwordTextField.textAlignment = .center
+        passwordTextField.borderStyle = .roundedRect
         
         nickNameTextField.backgroundColor = .darkGray
         nickNameTextField.attributedPlaceholder = NSAttributedString(string: "닉네임", attributes: [.foregroundColor: UIColor.white])
         nickNameTextField.textAlignment = .center
+        nickNameTextField.borderStyle = .roundedRect
         
         locationTextField.backgroundColor = .darkGray
         locationTextField.attributedPlaceholder = NSAttributedString(string: "위치", attributes: [.foregroundColor: UIColor.white])
         locationTextField.textAlignment = .center
+        locationTextField.borderStyle = .roundedRect
         
         recomandCodeTextField.backgroundColor = .darkGray
         recomandCodeTextField.attributedPlaceholder = NSAttributedString(string: "추천 코드 입력", attributes: [.foregroundColor: UIColor.white])
         recomandCodeTextField.keyboardType = .numberPad
         recomandCodeTextField.textAlignment = .center
+        recomandCodeTextField.borderStyle = .roundedRect
         
         
         //signUpButton 설정
         signUpButton.backgroundColor = .white
         signUpButton.setTitle("회원가입", for: .normal)
         signUpButton.setTitleColor(.black, for: .normal)
-        
+        signUpButton.layer.borderColor = UIColor.black.cgColor
+        signUpButton.layer.borderWidth = 2
+        signUpButton.layer.cornerRadius = 10
+
         addInfoButton.contentHorizontalAlignment = .left
+        
+        hideTextFieldSwitch.onTintColor = .red
+        hideTextFieldSwitch.thumbTintColor = .white
         
 
         
@@ -70,16 +83,27 @@ class SignUpViewController: UIViewController {
     
     @IBAction func hideTextFieldSwitchOn(_ sender: UISwitch) {
         //3개의 textfield를 숨겼다가 보여줌
-        print("switch on")
+        
+        if hideTextFieldSwitch.isOn {
+            nickNameTextField.isHidden = false
+            locationTextField.isHidden = false
+            recomandCodeTextField.isHidden = false
+        } else {
+            nickNameTextField.isHidden = true
+            locationTextField.isHidden = true
+            recomandCodeTextField.isHidden = true
+        }
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    @IBAction func emailNumTextFieldInput(_ sender: UITextField) {
+        let emailNum : String? = emailNumTextField.text
+        
+        p
+        
+        
     }
-    */
+    
+    
+    
 
 }
