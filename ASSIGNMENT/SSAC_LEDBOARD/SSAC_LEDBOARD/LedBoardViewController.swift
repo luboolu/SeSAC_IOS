0fdbe10a55ced7014f8f21cc14afe2332723b39f
luboolu <kjjy98@naver.com>
2021-10-04 20:39:38 +0900

    first commit

--- ASSIGNMENT/SSAC_LEDBOARD/SSAC_LEDBOARD/LedBoardViewController.swift
+++ ASSIGNMENT/SSAC_LEDBOARD/SSAC_LEDBOARD/LedBoardViewController.swift
@@ -17,29 +17,50 @@ class LedBoardViewController: UIViewController {
     @IBOutlet var LedBoardText: UILabel!
     
     
+    @IBOutlet var UIGestureRecognizer: UITapGestureRecognizer!
+    
+
+    @IBOutlet var boardView: UIView!
+    
+    
     let textColor: [UIColor] = [.white, .red, .orange, .yellow, .green, .blue ,.purple]
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //text field의 글자가 모두 보이는지 확인하고, 보이지 않는다면 글자의 크기를 조정하여 모두 보이게 함
+        if LedBoardText.adjustsFontSizeToFitWidth == false {
+            LedBoardText.adjustsFontSizeToFitWidth = true
+        }
+        
+        
 
     }
     
     
+    //send 버튼이 클릭되면 text field의 내용을 전광판에 띄움
     @IBAction func sendButtonClicked(_ sender: UIButton) {
         LedBoardText.text = inputLedBoardText.text
-
     }
     
-    @IBAction func keyboardReturnKeyClicked(_ sender: UITextField) {
-        //키보드가 내리기
-        view.endEditing(true)
-    
-    }
-    
+    //color change 버튼이 눌리면 text field 글자의 색을 랜덤으로 바꿈
     @IBAction func colorChangeButtonClicked(_ sender: UIButton) {
         LedBoardText.textColor = textColor[Int.random(in: 0...(textColor.count - 1))] 
     }
     
+
+    //tap gesture를 이용하여 키보드 내리기!
+    @IBAction func tapGesture(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+        
+        if boardView.isHidden == true{
+            boardView.isHidden = false
+        }else {
+            boardView.isHidden = true
+        }
+            
+        
+    }
     
     
 }
