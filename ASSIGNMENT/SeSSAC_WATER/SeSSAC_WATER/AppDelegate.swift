1559625cb3e32ce6ff02644b5d417f791cb18ebe
luboolu <kjjy98@naver.com>
2021-10-08 23:33:07 +0900

    first commit

--- ASSIGNMENT/SeSSAC_WATER/SeSSAC_WATER/AppDelegate.swift
+++ ASSIGNMENT/SeSSAC_WATER/SeSSAC_WATER/AppDelegate.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        IQKeyboardManager.shared.enable = true
+        
         sleep(3)
         
         return true
