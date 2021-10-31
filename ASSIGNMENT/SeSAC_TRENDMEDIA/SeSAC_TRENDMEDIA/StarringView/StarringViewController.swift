bb19e8c6dd471d550cf101881464fef713aba8d3
luboolu <kjjy98@naver.com>
2021-10-31 22:08:07 +0900

    first commit

--- ASSIGNMENT/SeSAC_TRENDMEDIA/SeSAC_TRENDMEDIA/StarringView/StarringViewController.swift
+++ ASSIGNMENT/SeSAC_TRENDMEDIA/SeSAC_TRENDMEDIA/StarringView/StarringViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Network
+
 import Kingfisher
 
 class StarringViewController: UIViewController {
@@ -14,6 +16,8 @@ class StarringViewController: UIViewController {
     var castInfo: [TMDBGetCastModel] = []
     var crewInfo: [TMDBGetCastModel] = []
     var overviewExpanse = false
+    
+    let networkMonitor = NWPathMonitor() //네트워크 변경 감시자
     
     @IBOutlet weak var headerView: UIView!
     
@@ -24,6 +28,25 @@ class StarringViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //네트워크 변경 감지 클래스를 통해 사용자의 네트워크 상태가 변경될 때 마다 실행
+        networkMonitor.pathUpdateHandler = { path in
+            //클로져 구문
+            if path.status == .satisfied {
+                print("Network Connected")
+                
+                if path.usesInterfaceType(.cellular) {
+                    print("Cellular Status")
+                } else if path.usesInterfaceType(.wifi) {
+                    print("Cellular Wifi")
+                } else {
+                    print("Other")
+                }
+            } else {
+                print("Network Disconnected")
+            }
+        }
+        networkMonitor.start(queue: DispatchQueue.global())
         
         actorTableView.delegate = self
         actorTableView.dataSource = self
