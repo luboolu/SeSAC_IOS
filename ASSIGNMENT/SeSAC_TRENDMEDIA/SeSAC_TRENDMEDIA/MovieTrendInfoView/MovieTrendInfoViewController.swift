a756d2179974389c8bce7b8cbc98ac2e68c25340
luboolu <kjjy98@naver.com>
2021-10-31 22:11:23 +0900

    first commit

--- ASSIGNMENT/SeSAC_TRENDMEDIA/SeSAC_TRENDMEDIA/MovieTrendInfoView/MovieTrendInfoViewController.swift
+++ ASSIGNMENT/SeSAC_TRENDMEDIA/SeSAC_TRENDMEDIA/MovieTrendInfoView/MovieTrendInfoViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Network
+
 import Kingfisher
 
 class MovieTrendInfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UITableViewDataSourcePrefetching {
@@ -27,6 +29,8 @@ class MovieTrendInfoViewController: UIViewController, UITableViewDelegate, UITab
     
     let tvShowInformation = TvShowInformation()
     
+    let networkMonitor = NWPathMonitor() //네트워크 변경 감시자
+    
     @IBOutlet weak var HeaderView: UIView!
     @IBOutlet weak var mainButtonView: UIView!
     @IBOutlet weak var InfoTableView: UITableView!
@@ -37,6 +41,26 @@ class MovieTrendInfoViewController: UIViewController, UITableViewDelegate, UITab
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //네트워크 변경 감지 클래스를 통해 사용자의 네트워크 상태가 변경될 때 마다 실행
+        networkMonitor.pathUpdateHandler = { path in
+            //클로져 구문
+            if path.status == .satisfied {
+                print("Network Connected")
+                
+                if path.usesInterfaceType(.cellular) {
+                    print("Cellular Status")
+                } else if path.usesInterfaceType(.wifi) {
+                    print("Cellular Wifi")
+                } else {
+                    print("Other")
+                }
+            } else {
+                print("Network Disconnected")
+            }
+        }
+        
+        networkMonitor.start(queue: DispatchQueue.global())
         
         InfoTableView.delegate = self
         InfoTableView.dataSource = self
--- ASSIGNMENT/SeSAC_TRENDMEDIA/SeSAC_TRENDMEDIA/StarringView/StarringViewController.swift
+++ ASSIGNMENT/SeSAC_TRENDMEDIA/SeSAC_TRENDMEDIA/StarringView/StarringViewController.swift
@@ -17,8 +17,6 @@ class StarringViewController: UIViewController {
     var crewInfo: [TMDBGetCastModel] = []
     var overviewExpanse = false
     
-    let networkMonitor = NWPathMonitor() //네트워크 변경 감시자
-    
     @IBOutlet weak var headerView: UIView!
     
     @IBOutlet weak var mainPosterImageView: UIImageView!
@@ -28,25 +26,6 @@ class StarringViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        //네트워크 변경 감지 클래스를 통해 사용자의 네트워크 상태가 변경될 때 마다 실행
-        networkMonitor.pathUpdateHandler = { path in
-            //클로져 구문
-            if path.status == .satisfied {
-                print("Network Connected")
-                
-                if path.usesInterfaceType(.cellular) {
-                    print("Cellular Status")
-                } else if path.usesInterfaceType(.wifi) {
-                    print("Cellular Wifi")
-                } else {
-                    print("Other")
-                }
-            } else {
-                print("Network Disconnected")
-            }
-        }
-        networkMonitor.start(queue: DispatchQueue.global())
         
         actorTableView.delegate = self
         actorTableView.dataSource = self
