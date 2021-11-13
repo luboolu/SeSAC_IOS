805fec1d5c085ff51cc9790c57bea328df7c0a80
luboolu <kjjy98@naver.com>
2021-11-13 16:00:17 +0900

    first commit

--- ASSIGNMENT/test.playground/Contents.swift
+++ ASSIGNMENT/test.playground/Contents.swift
@@ -1,5 +1,7 @@
 import UIKit
 import Darwin
+import DataDetection
+
 
 //var my = "123s"
 //
@@ -52,15 +54,77 @@ guardTest()
 
 
 
-let now = Date()
-let dateFomatter = DateFormatter()
+//let now = Date()
+//let dateFomatter = DateFormatter()
+//
+//dateFomatter.dateFormat = "yyyyMMdd"
+//
+//let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: now)!
+//
+//let searchDate = dateFomatter.string(from: yesterday)
+//
+//type(of: searchDate)
+//
 
-dateFomatter.dateFormat = "yyyyMMdd"
+extension Date {
+    var startOfWeek: Date? {
+        let gregorian = Calendar(identifier: .gregorian)
+        guard let startDay = gregorian.date(from: gregorian.dateComponents([.yearForWeekOfYear, .weekOfYear], from: self)) else { return nil }
+        return gregorian.date(byAdding: .day, value: 1, to: startDay)
+    }
 
-let yesterday = Calendar.current.date(byAdding: .day, value: -1, to: now)!
+    var endOfWeek: Date? {
+        let gregorian = Calendar(identifier: .gregorian)
+            guard let startDay = gregorian.date(from: gregorian.dateComponents([.yearForWeekOfYear, .weekOfYear], from: self)) else { return nil }
+           return gregorian.date(byAdding: .day, value: 7, to: startDay)
+       }
+}
 
-let searchDate = dateFomatter.string(from: yesterday)
+func convertDateToLocalTime(_ date: Date) -> Date {
+    let timeZoneOffset = Double(TimeZone.current.secondsFromGMT(for: date))
+    print(timeZoneOffset)
+    return Calendar.current.date(byAdding: .second, value: Int(timeZoneOffset), to: date)!
+}
 
-type(of: searchDate)
+let now = convertDateToLocalTime(Date())
+
+let startWeek = convertDateToLocalTime(now.startOfWeek!)
+let endWeek = convertDateToLocalTime(now.endOfWeek!)
 
 
+let dateFormatter = DateFormatter()
+dateFormatter.locale = Locale(identifier: "ko_KR")
+dateFormatter.timeZone = TimeZone(abbreviation: "KST")
+dateFormatter.dateFormat = "yyyy MM dd HH:mm"
+
+let nowKR = dateFormatter.string(from: Date())
+print(nowKR)
+
+print("Local time is: \(now)")
+
+print("Start of week is: \(startWeek)")
+
+print("End of week is: \(endWeek)")
+
+let weekDateFormatter = DateFormatter()
+weekDateFormatter.locale = Locale(identifier: "ko_KR")
+weekDateFormatter.timeZone = TimeZone(abbreviation: "KST")
+weekDateFormatter.dateFormat = "EEEE"
+print(weekDateFormatter.string(from: now))
+
+let todayDateFormatter = DateFormatter()
+todayDateFormatter.locale = Locale(identifier: "ko_KR")
+todayDateFormatter.timeZone = TimeZone(abbreviation: "KST")
+todayDateFormatter.dateFormat = "a hh:mm"
+print(todayDateFormatter.string(from: Date()))
+
+
+let calendar = Calendar.current
+let today = Date()
+let localDate = Date(timeInterval: TimeInterval(calendar.timeZone.secondsFromGMT()), since: today)
+
+if calendar.compare(today, to: localDate, toGranularity: .day) == .orderedSame {
+    print("This is True")
+} else {
+    print("This is False")
+}
