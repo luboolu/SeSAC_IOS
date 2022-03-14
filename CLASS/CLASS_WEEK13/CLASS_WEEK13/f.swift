68d2938418434927d831e7bb0e0fd81592b12279
luboolu <kjjy98@naver.com>
2022-03-14 23:38:57 +0900

    git ignore

--- CLASS/CLASS_WEEK13/CLASS_WEEK13/f.swift
+++
@@ -1,8 +0,0 @@
-//
-//  f.swift
-//  CLASS_WEEK13
-//
-//  Created by 김진영 on 2021/12/23.
-//
-
-import Foundation
--- CLASS/CLASS_WEEK7_cocoapod/Pods/Target Support Files/Pods-CLASS_WEEK7_cocoapod/Pods-CLASS_WEEK7_cocoapod-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CLASS_WEEK7_cocoapod : NSObject
-@end
-@implementation PodsDummy_Pods_CLASS_WEEK7_cocoapod
-@end
